cd038dd8a22d834be1dd10567bc059abe3c83c96
Hiroaki Yamamoto <my1NGtr0d5p9fAEHZTw6id3rCGfB1mPJ@hysoftware.net>
2023-11-06 14:14:52 +0900

    chore(contracts): :sparkles: Add id

--- contracts/schema/gen.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/env sh
-# -*- coding: utf-8 -*-
-
-DIR=$(realpath $(dirname "$0"))
-NAME=${1/_/ }
-NAME=${NAME^}
-
-$DIR/../../deps/yq/bin/yq ".title = \"$NAME Schema\"" template.schema.yml > $DIR/$1.schema.yml
